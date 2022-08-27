352bf939c7e07d330fa7bb0fd367d87929ad44ac
nayu7u <yusuke.nadaka@gmail.com>
2022-08-28 02:36:28 +0900

    test: privateメソッドを無理やりテストした

--- test/test_merge_sort.rb
+++ test/test_merge_sort.rb
@@ -10,4 +10,18 @@ class TestMergeSort < Minitest::Test
 
     assert_equal(array.sort, sorted)
   end
+
+  def test_split
+    merge_sort_obj = MergeSort.new
+    merge_sort_obj.instance_eval do
+      def temp_split(arg)
+        split(arg)
+      end
+    end
+
+    array = [5,4,3,2,1]
+    splited = merge_sort_obj.temp_split(array)
+
+    assert_equal([[5,4],[3,2,1]], splited)
+  end
 end
